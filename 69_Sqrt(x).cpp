e467aff05d2644ba1fdd9913c6240e35a1c1b5f0
Md. Rakib Hassan <md.rakibhassan4591@gmail.com>
2026-02-19 13:40:44 +0600

    Solved 69_Sqrt(x)

---
+++ 69_Sqrt(x).cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/sqrtx
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2)return x;
+        int l=1,r=x/2;
+        while(l<=r){
+            long long mid=l+(r-l)/2;
+            if(mid*mid==x)return mid;
+            else if(mid*mid>x)r=mid-1;
+            else l=mid+1;
+        }
+        
+        return r;
+
+    }
+};
