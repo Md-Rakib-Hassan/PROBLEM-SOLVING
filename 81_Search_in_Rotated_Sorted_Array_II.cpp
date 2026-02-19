70d00d108d1a31ac964555edba15eb508635acb2
Md. Rakib Hassan <md.rakibhassan4591@gmail.com>
2026-02-20 03:42:53 +0600

    Solved 81_Search_in_Rotated_Sorted_Array_II

---
+++ 81_Search_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int t) {
+        int l=0,r=nums.size()-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(nums[m]==t)return true;
+            if(nums[l]<nums[m]){
+                if(t>=nums[l] && t<nums[m])r=m-1;
+                else l=m+1;
+            }
+            else if(nums[l]>nums[m]){
+                if(t>nums[m] && t<=nums[r])l=m+1;
+                else r=m-1;
+            }
+            else l++;
+        }
+        return false;
+    }
+};
